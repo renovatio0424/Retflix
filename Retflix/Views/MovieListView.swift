3599c85294556369edcbf2fbe69326892c114339
Reno <renovatio0424@gmail.com>
2020-11-03 18:56:27 +0900

    refactoring MovieListView
    extract build View function

--- Retflix/Views/MovieListView.swift
+++ Retflix/Views/MovieListView.swift
@@ -9,57 +9,48 @@ import SwiftUI
 
 struct MovieListView: View {
     
-    @ObservedObject private var nowPlayingState = MovieListState()
-    @ObservedObject private var upcomingState = MovieListState()
-    @ObservedObject private var topRatedState = MovieListState()
-    @ObservedObject private var popularState = MovieListState()
+    @ObservedObject private var nowPlayingState = MovieViewModel()
+    @ObservedObject private var upcomingState = MovieViewModel()
+    @ObservedObject private var topRatedState = MovieViewModel()
+    @ObservedObject private var popularState = MovieViewModel()
+    
+    fileprivate func buildPosterView(title: String, viewModel: MovieViewModel, endPoint: MovieListEndPoint) -> some View {
+        return Group {
+            if nowPlayingState.movies != nil {
+                MoviePosterCarouselView(title: title, movies: viewModel.movies!)
+            } else {
+                LoadingView(isLoading: viewModel.isLoading, error: viewModel.error) {
+                    viewModel.loadMovies(with: endPoint)
+                }
+            }
+        }
+        .listRowInsets(EdgeInsets(top: 16, leading: 0, bottom: 8, trailing: 0))
+    }
+    
+    
+    fileprivate func buildCardView(title: String, viewModel: MovieViewModel, endPoint: MovieListEndPoint) -> some View {
+        return Group {
+            if upcomingState.movies != nil {
+                MovieBackdropCarouselView(title: title, movies: viewModel.movies!)
+            } else {
+                LoadingView(isLoading: viewModel.isLoading, error: viewModel.error) {
+                    viewModel.loadMovies(with: endPoint)
+                }
+            }
+        }
+        .listRowInsets(EdgeInsets(top: 8, leading: 0, bottom: 8, trailing: 0))
+    }
     
     var body: some View {
         NavigationView {
             List {
-                Group {
-                    if nowPlayingState.movies != nil {
-                        MoviePosterCarouselView(title: "Now Playing", movies: nowPlayingState.movies!)
-                    } else {
-                        LoadingView(isLoading: nowPlayingState.isLoading, error: nowPlayingState.error) {
-                            self.nowPlayingState.loadMovies(with: .nowPlaying)
-                        }
-                    }
-                }
-                .listRowInsets(EdgeInsets(top: 16, leading: 0, bottom: 8, trailing: 0))
+                buildPosterView(title: "Now Playing", viewModel: nowPlayingState, endPoint: .nowPlaying)
                 
-                Group {
-                    if upcomingState.movies != nil {
-                        MovieBackdropCarouselView(title: "Upcoming", movies: upcomingState.movies!)
-                    } else {
-                        LoadingView(isLoading: nowPlayingState.isLoading, error: upcomingState.error) {
-                            self.nowPlayingState.loadMovies(with: .upcoming)
-                        }
-                    }
-                }
-                .listRowInsets(EdgeInsets(top: 8, leading: 0, bottom: 8, trailing: 0))
+                buildCardView(title: "Upcoming", viewModel: upcomingState, endPoint: .upcoming)
                 
-                Group {
-                    if topRatedState.movies != nil {
-                        MovieBackdropCarouselView(title: "Top Rated", movies: topRatedState.movies!)
-                    } else {
-                        LoadingView(isLoading: nowPlayingState.isLoading, error: topRatedState.error) {
-                            self.nowPlayingState.loadMovies(with: .topRated)
-                        }
-                    }
-                }
-                .listRowInsets(EdgeInsets(top: 8, leading: 0, bottom: 8, trailing: 0))
+                buildCardView(title: "Top Rated", viewModel: topRatedState, endPoint: .topRated)
                 
-                Group {
-                    if popularState.movies != nil {
-                        MovieBackdropCarouselView(title: "Popular", movies: popularState.movies!)
-                    } else {
-                        LoadingView(isLoading: nowPlayingState.isLoading, error: popularState.error) {
-                            self.nowPlayingState.loadMovies(with: .popular)
-                        }
-                    }
-                }
-                .listRowInsets(EdgeInsets(top: 8, leading: 0, bottom: 8, trailing: 0))
+                buildCardView(title: "Popular", viewModel: popularState, endPoint: .popular)
             }
             .navigationTitle("Retflix")
         }
